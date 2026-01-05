602a7d730f9d37a126fbaa8701c90349b51978a8
Sandra Toolan <sandratoolan001@gmail.com>
2026-01-06 00:58:26 +0200

    Campaign and campaign item doctypes

---
+++ link/link/doctype/campaign_item/campaign_item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Practial Toys and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Campaign Item", {
+// 	refresh(frm) {
+
+// 	},
+// });
